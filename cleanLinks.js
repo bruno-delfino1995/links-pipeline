664eb30ad9dfe12bd2578aa6e88baafbe82129da
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2018-06-04 19:00:01 -0300

    Add a helper to remove repeated code for files handling

--- cleanLinks.js
+++ cleanLinks.js
@@ -4,9 +4,6 @@ const urlParse = require('url-parse')
 const links = require('./links.json')
 
 const hrefLens = R.lensProp('href')
-const typeLens = R.lensProp('type')
-const queryLens = R.lensProp('query')
-const searchLens = R.lensProp('term')
 
 const parseUrl = href => urlParse(href, true)
 
@@ -22,24 +19,7 @@ const removeUselessSites = (url) => {
 	)
 }
 
-const isGoogle = (href) => R.startsWith('www.google.com', urlParse(href).hostname)
-
-const getSearch = (url) => {
-	const query = R.view(queryLens, url) || {}
-
-	return decodeURIComponent(query.q || '')
-}
-
 const urls = R.compose(
-//	R.map(R.when(
-//		R.compose(isGoogle, R.view(hrefLens)),
-//		link => R.set(
-//			searchLens, 
-//			R.compose(getSearch, parseUrl, R.view(hrefLens))(link),
-//			R.set(typeLens, 'search', link)
-//		)
-//	)),
-//	R.map(R.set(typeLens, 'url')),
 	R.reject(R.compose(removeUselessSites, parseUrl, R.view(hrefLens)))
 )(links)
 
---
+++ fileHelpers.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+module.exports.write = ({ path, contents }) => new Promise((res, rej) => 
+	fs.writeFile(path, contents, (err) => {
+		if (err) {
+			rej(err)
+			return
+		}
+
+		res()
+	});
+)
+
+module.exports.read = ({ path, encoding }) => new Promise((res, rej) =>
+	fs.readFile(path, encoding, (err, data) => {
+		if (err) {
+			rej(err)
+			return
+		}
+
+		res(data)
+	})
+)
