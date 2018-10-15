43a7415fb7ad7798d7f4dce0174135b97daad7be
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2018-10-14 23:31:28 -0300

    Organize bookmarks extraction script

--- extractFromBookmarks.js
+++ extractFromBookmarks.js
@@ -2,37 +2,42 @@ const cheerio = require('cheerio')
 const R = require('ramda')
 const readAll = require('./readAll')
 
+const getHrefs = R.map(el => el.attr('href'))
+const getFolder = el => el.prev('dt > h3')
+
+const toArray = ($, list) => {
+	const arr = []
+
+	list.map((i, el) => {
+		arr.push($(el))
+		return el
+	})
+
+	return arr
+}
+
+const extractBookmarks = $ => dom => {
+	const parents = toArray($, dom.parents())
+	const folders = R.map(el => getFolder(el), parents)
+	const folderTitles = R.reverse(R.map(el => el.text(), [...toArray($, getFolder(dom)), ...folders]))
+
+	const titles = R.without(['Bookmarks bar'], R.reject(R.isEmpty, folderTitles))
+	const path = R.reject(t => /window \d/i.test(t), titles)
+
+	const elements = toArray($, dom.find('> dt > a'))
+	const links = getHrefs(elements)
+
+	return R.map(href => ({path, href}), links)
+}
+
 const main = (data) => {
 	const $ = cheerio.load(data)
 
-	const toArray = ($, list) => {
-		const arr = []
-
-		list.map((i, el) => {
-			arr.push($(el))
-			return el
-		})
-
-		return arr
-	}
-
-	const getHrefs = R.map(el => el.attr('href'))
-	const getFolder = el => el.prev('dt > h3')
-
-	const sessions = $('dl')
-	const bookmarks = R.flatten(R.map((dom) => {
-		const parents = toArray($, dom.parents())
-		const folders = R.map(el => getFolder(el), parents)
-		const folderTitles = R.reverse(R.map(el => el.text(), [...toArray($, getFolder(dom)), ...folders]))
-
-		const titles = R.without(['Bookmarks bar'], R.reject(R.isEmpty, folderTitles))
-		const path = R.reject(t => /window \d/i.test(t), titles)
-
-		const elements = toArray($, dom.find('> dt > a'))
-		const links = getHrefs(elements)
-
-		return R.map(href => ({path, href}), links)
-	}, toArray($, sessions)))
+	const sessions = toArray($, $('dl'))
+	const bookmarks = R.compose(
+		R.flatten,
+		R.map(extractBookmarks($))
+	)(sessions)
 
 	const links = R.compose(
 		R.map(({path, href}) => ({href, tags: R.join('⨝', path)})),
