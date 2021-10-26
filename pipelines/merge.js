5348d2ec99f9a98e4d4c1df9837bf39ccbf8aa33
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2021-10-25 23:50:18 -0300

    Add merge pipeline

---
+++ pipelines/merge.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const R = require('ramda')
+const Rxo = require('rxjs/operators')
+const Rx = require('rxjs')
+
+const fromJsonl = require('../extractors/fromJsonl')
+
+const uniq = require('../transformers/uniq')
+const normalize = require('../transformers/normalize')
+
+module.exports = {
+  command: 'merge <files..>',
+  desc: 'Merges all JSONL files into one',
+  builder: (y) => {
+    y.positional('files', {
+      desc: 'Paths to files with links',
+      type: 'array'
+    })
+  },
+  handler: (argv) => {
+    const asObservable = R.compose(
+      fromJsonl,
+      path.resolve
+    )
+
+    const observable = R.compose(
+      R.apply(Rx.merge),
+      R.map(asObservable)
+    )(argv.files)
+
+    return observable
+      .pipe(...normalize)
+      .pipe(...uniq)
+  }
+}
